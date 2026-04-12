8ac72c3e88e81494f5c7b9802d255bda561f80ad
Prashanth G <prashanthg008@gmail.com>
2026-04-12 15:48:08 +0530

    feat: add HomePage composing all portfolio sections

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+const HomePage = () => (
+  <>
+    <Hero />
+    <About />
+    <Skills />
+    <Contact />
+    <Footer />
+  </>
+)
+
+export default HomePage
